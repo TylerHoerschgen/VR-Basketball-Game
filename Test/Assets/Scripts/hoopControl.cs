a63d393ad7eac3a1509f79fa837f4436409b70f5
Tyler Hoerschgen <bballhero24t@gmail.com>
2021-04-28 19:46:58 -0500

    Working hoop

---
+++ Test/Assets/Scripts/hoopControl.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hoopControl : MonoBehaviour
+{
+    public float score=0;
+    public TextMesh scoreText;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("collison");
+        Destroy(collision.gameObject);
+        score+=0.5f;
+        scoreText.text = "Score:" + score.ToString();
+    }
+}
---
+++ Test/Assets/Scripts/spawnBall.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnBall : MonoBehaviour
+{
+    public GameObject ball;
+    // Start is called before the first frame update
+    public void generateBall()
+    {
+        GameObject createdBall = Instantiate(ball);
+
+        createdBall.transform.position = new Vector3(-2.487f, 1.627f, 2.96f);
+        createdBall.GetComponent<Rigidbody>().AddForce(new Vector3(9.0f, 5.0f, -10.0f), ForceMode.Impulse);
+    }
+
+    public void Update()
+    {
+        
+    }
+}
